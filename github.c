215347a517464bf3b483fc2a3b87af0b154e30d3
hy16924 <hhyy16924@naver.com>
2020-06-03 20:30:58 +0900

    덧셈, 뺄셈, 나눗셈

--- github.c
+++ github.c
@@ -1,5 +1,25 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main(){
 	printf("hello world");
 	return 0;
+}*/
+
+#include <stdio.h>
+int main(){
+	int x, y, z;
+	printf("Enter 1(+) or 2(-) or 3(/): ");
+	scanf("%d", &z);
+	printf("Enter number: ");
+	scanf("%d %d", &x, &y);
+	if(z == 1){
+	    printf("%d + %d = %d", x, y, x+y);
+	}
+	else if(z == 2){
+	    printf("%d - %d = %d", x, y, x-y);
+	}
+	else if(z == 3){
+	    printf("%f / %f = %f", (float)x, (float)y, (float)x/y);
+	}
+	return 0;
 }
+	
